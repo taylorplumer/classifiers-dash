3d4d03cef50555be328bda3698d53ff8a26e6eff
Taylor Plumer <45341982+taylorplumer@users.noreply.github.com>
2020-05-25 12:30:57 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 
 # modify depending on needs for sklearn classifiers and yellowbrick visualizers
 MODELS = [GradientBoostingClassifier(), RandomForestClassifier(), LogisticRegression(max_iter=1000), GaussianNB() ]
-#models = [GradientBoostingClassifier(), RandomForestClassifier()]
 VISUALIZERS = ['ROCAUC','PrecisionRecallCurve', 'ClassificationReport','ConfusionMatrix']
 
+# modify depending on needs for filesystem structure
 INPUT_DATA_FILEPATH = 'Data/Input/'
 
 OUTPUT_DATA_FILEPATH = 'Data/Output/'
