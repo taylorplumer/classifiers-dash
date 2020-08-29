065741edbf6ab66011d37d474b60314e6ccae515
taylorplumer <taylorplumer@gmail.com>
2020-08-28 21:52:58 -0700

    Add docstrings to visualizers.py

--- utils/visualizers.py
+++ utils/visualizers.py
@@ -7,6 +7,21 @@ from yellowbrick.classifier import ConfusionMatrix
 
 class Visualizer():
     def __init__(self, X_train, X_test, y_train, y_test, labels, model, viz_selection, upsampled=False):
+        """
+        Class for yellowbrick classifier visualizer
+
+        Args:
+            X_train: numpy ndarray of model features training data values
+            X_test: numpy ndarray of model features test data values
+            y_train: numpy ndarray of model target variable training data values
+            y_test: numpy ndarray of model target variable test data values
+            labels: list of class labels for binary classification
+            model: sklearn estimator for classification
+            viz_selection: string value used to reference yellowbrick classification visualizer
+            upsampled: binary value to determine to which subdirectory output image should be saved
+
+        """
+
         self.labels = labels
         self.model = model
         self.viz_selection = viz_selection
@@ -28,10 +43,20 @@ class Visualizer():
 
 
     def evaluate(self):
+        """
+        Fit and score model associated with visualizer
+        
+        """
         self.visualizer.fit(self.X_train, self.y_train)
         self.visualizer.score(self.X_test, self.y_test)
 
     def save_img(self):
+        """
+        Save image output of visualizer to output directory
+
+        Returns:
+            matplotlib image saved as png
+        """
         if self.upsampled == True:
             self.outpath_ = IMG_OUTPUT_FILEPATH + str(self.model).split('(')[0] + '/' + self.viz_selection + '_upsampled.png'
         else:
